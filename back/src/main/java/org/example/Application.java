9e06724b89291949e7f4e6f09103b6965cb99849
w-chen <2499641477@qq.com>
2022-08-23 01:45:03 +0800

    项目提交

---
+++ back/src/main/java/org/example/Application.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("org.example.mapper")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ back/src/main/java/org/example/mapper/StudentMapper.java
@@ -0,0 +1,9 @@
+package org.example.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.example.pojo.Student;
+
+@Mapper
+public interface StudentMapper extends BaseMapper<Student> {
+}
